8c1ed4dba298e8434fcb88af52b77a084e8b6c84
Archiker-715 <serw144@gmail.com>
2026-02-10 13:16:10 +0300

    auth middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,48 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Archiker-715/e-commerce-api/internal/auth"
+	"github.com/Archiker-715/e-commerce-api/internal/errs"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			errs.WriteError(w, 0, http.StatusUnauthorized, "empty token")
+			return
+		}
+
+		token, err := auth.ParseToken(strings.TrimPrefix(authHeader, "Bearer "))
+		if err != nil {
+			errs.WriteError(w, 0, http.StatusUnauthorized, "invalid token")
+			return
+		}
+		ctx := auth.UserToCtx(r.Context(), getUUID(token))
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUUID(token *jwt.Token) uuid.UUID {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return uuid.Nil
+	}
+
+	subClaim, ok := claims["user_id"].(string)
+	if !ok {
+		return uuid.Nil
+	}
+
+	userId, err := uuid.Parse(subClaim)
+	if err != nil {
+		return uuid.Nil
+	}
+	return userId
+}
