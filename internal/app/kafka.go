0fb4ab2d0ad9c0daa5276e446b785d3a757a4af6
Archiker-715 <serw144@gmail.com>
2026-02-25 22:26:03 +0300

    fix

--- internal/app/kafka.go
+++ internal/app/kafka.go
@@ -3,11 +3,11 @@ package app
 import "github.com/Archiker-715/e-commerce-api/internal/kafka"
 
 func (a *app) startKafkaConsumers() {
-	kafkaHandler := kafka.NewKafkaOrderHandler(
+	kafkaOrderHandler := kafka.NewKafkaOrderHandler(
 		a.Services.OrderService,
 		kafka.NewKafkaConsumerClient(
 			kafka.InitReader().NewKafkaReader("paid-orders", "localhost", "test-group"),
 		),
 	)
-	kafkaHandler.Start()
+	kafkaOrderHandler.Start()
 }
